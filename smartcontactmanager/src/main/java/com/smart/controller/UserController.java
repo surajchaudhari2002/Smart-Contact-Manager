ee4d4f5d3d9a85213612afd9baab862159e1de2a
surajchaudhari2002 <chaudharisuraj2002@gmail.com>
2024-05-31 17:10:45 +0530

    user dashboard is desighned

--- smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -3,14 +3,35 @@ package com.smart.controller;
 
 import java.security.Principal;
 
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    // method for adding common data to response
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal) {
+        String userName = principal.getName();
+        System.out.println("USERNAME " + userName);
+
+        // get the user using usernamne(Email)
+
+        User user = userRepository.getUserByUserName(userName);
+        System.out.println("USER " + user);
+        model.addAttribute("user", user);
+
+    }
+
     // dashboard home
     @RequestMapping("/index")
     public String dashboard(Model model, Principal principal) {
