2d2e7805a4cec842d34ef2a7a7cca821081d5d05
surajchaudhari2002 <chaudharisuraj2002@gmail.com>
2024-06-01 14:55:54 +0530

    contact form added

--- smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -4,10 +4,12 @@ package com.smart.controller;
 import java.security.Principal;
 
 import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -37,5 +39,14 @@ public class UserController {
     public String dashboard(Model model, Principal principal) {
         model.addAttribute("title", "User Dashboard");
         return "normal/user_dashboard";
+    }
+
+    // open add form handler
+    @GetMapping("/add-contact")
+    public String openAddContactForm(Model model) {
+        model.addAttribute("title", "Add Contact");
+        model.addAttribute("contact", new Contact());
+
+        return "normal/add_contact_form";
     }
 }
