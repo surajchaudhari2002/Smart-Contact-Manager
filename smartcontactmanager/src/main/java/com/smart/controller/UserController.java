95a8dd7eae93a8cc530ca3425c7e144259af24fd
surajchaudhari2002 <chaudharisuraj2002@gmail.com>
2024-06-05 17:06:15 +0530

    forgot password module

--- smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -19,6 +19,7 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
@@ -31,6 +32,9 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
     @Autowired
     private UserRepository userRepository;
 
@@ -264,5 +268,39 @@ public class UserController {
         model.addAttribute("title", "Profile Page");
         return "normal/profile";
     }
+    // open settings handler
+    @GetMapping("/settings")
+    public String openSettings() {
+        return "normal/settings";
+    }
+
+    // change password..handler
+    @PostMapping("/change-password")
+    public String changePassword(@RequestParam("oldPassword") String oldPassword,
+                                 @RequestParam("newPassword") String newPassword, Principal principal, HttpSession session) {
+        System.out.println("OLD PASSWORD " + oldPassword);
+        System.out.println("NEW PASSWORD " + newPassword);
+
+        String userName = principal.getName();
+        User currentUser = this.userRepository.getUserByUserName(userName);
+        System.out.println(currentUser.getPassword());
+
+        if (this.bCryptPasswordEncoder.matches(oldPassword, currentUser.getPassword())) {
+            // change the password
+
+            currentUser.setPassword(this.bCryptPasswordEncoder.encode(newPassword));
+            this.userRepository.save(currentUser);
+            session.setAttribute("message", new Message("Your password is successfully changed..", "success"));
+
+        } else {
+            // error...
+            session.setAttribute("message", new Message("Please Enter correct old password !!", "danger"));
+            return "redirect:/user/settings";
+        }
+
+        return "redirect:/user/index";
+    }
+
+
 
 }
