a93bbe10c1b123ef413041eac2fad7f33ce3d16a
surajchaudhari2002 <chaudharisuraj2002@gmail.com>
2024-06-17 20:11:32 +0530

    payment integration

--- smartcontactmanager/target/classes/static/js/script.js
+++ smartcontactmanager/target/classes/static/js/script.js
@@ -1,4 +1,4 @@
-console.log("This is script file")
+console.log("this is script file");
 
 const toggleSidebar = () => {
   if ($(".sidebar").is(":visible")) {
@@ -51,3 +51,84 @@ const search = () => {
   }
 };
 
+//first request- to server to create order
+
+const paymentStart = () => {
+  console.log("payment started..");
+  var amount = $("#payment_field").val();
+  console.log(amount);
+  if (amount == "" || amount == null) {
+    // alert("amount is required !!");
+    swal("Failed !!", "amount is required !!", "error");
+    return;
+  }
+
+  //code...
+  // we will use ajax to send request to server to create order- jquery
+
+  $.ajax({
+    url: "/user/create_order",
+    data: JSON.stringify({ amount: amount, info: "order_request" }),
+    contentType: "application/json",
+    type: "POST",
+    dataType: "json",
+    success: function (response) {
+      //invoked when success
+      console.log(response);
+      if (response.status == "created") {
+        //open payment form
+        let options = {
+          key: "rzp_test_haDRsJIQo9vFPJ",
+          amount: response.amount,
+          currency: "INR",
+          name: "Smart Contact Manager",
+          description: "Donation",
+          image:
+            "https://yt3.ggpht.com/-4BGUu55s_ko/AAAAAAAAAAI/AAAAAAAAAAA/3Cfl_C4o8Uo/s108-c-k-c0x00ffffff-no-rj-mo/photo.jpg",
+          order_id: response.id,
+          handler: function (response) {
+            console.log(response.razorpay_payment_id);
+            console.log(response.razorpay_order_id);
+            console.log(response.razorpay_signature);
+            console.log("payment successful !!");
+            // alert("congrates !! Payment successful !!");
+            swal("Good job!", "congrates !! Payment successful !!", "success");
+          },
+          prefill: {
+            name: "",
+            email: "",
+            contact: "",
+          },
+
+          notes: {
+            address: "LearnCodeWith Durgesh ",
+          },
+          theme: {
+            color: "#3399cc",
+          },
+        };
+
+        let rzp = new Razorpay(options);
+
+        rzp.on("payment.failed", function (response) {
+          console.log(response.error.code);
+          console.log(response.error.description);
+          console.log(response.error.source);
+          console.log(response.error.step);
+          console.log(response.error.reason);
+          console.log(response.error.metadata.order_id);
+          console.log(response.error.metadata.payment_id);
+          //alert("Oops payment failed !!");
+          swal("Failed !!", "Oops payment failed !!", "error");
+        });
+
+        rzp.open();
+      }
+    },
+    error: function (error) {
+      //invoked when error
+      console.log(error);
+      alert("something went wrong !!");
+    },
+  });
+};
